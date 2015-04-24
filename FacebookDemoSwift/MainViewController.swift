4820bf7bd2bcb1701dd86a7ee18ac5ec3db62dee
Amie Kweon <amie.kweon@rd.io>
2015-04-23 17:07:52 -0700

    Initial commit

---
+++ FacebookDemoSwift/MainViewController.swift
@@ -0,0 +1,42 @@
+//
+//  MainViewController.swift
+//  FacebookDemoSwift
+//
+//  Created by Timothy Lee on 2/11/15.
+//  Copyright (c) 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        reload()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func reload() {
+        FBRequestConnection.startWithGraphPath("/me/home", parameters: nil, HTTPMethod: "GET") { (connection: FBRequestConnection!, result: AnyObject!, error: NSError!) -> Void in
+            println("\(result)")
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
